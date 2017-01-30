a1ffc88a6f17d918f54a41fe733f6f39b87595cb
achuth <achuth@local>
2017-01-30 18:06:10 +0530

    Corrected name error.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo docker rmi achuthman/helloworld --force
 sudo docker rm myHelloWorld --force
-sudo docker run -d -p 80:80 achuthman/helloworld myHelloWorld
+sudo docker run -d -p 80:80 achuthman/helloworld --name myHelloWorld
