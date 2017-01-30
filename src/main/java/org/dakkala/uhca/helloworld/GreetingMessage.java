ea4b214210e3ead8decf611c7a5f5c579f11178e
achuth <achuth@local>
2017-01-30 21:19:33 +0530

    ..

--- src/main/java/org/dakkala/uhca/helloworld/GreetingMessage.java
+++ src/main/java/org/dakkala/uhca/helloworld/GreetingMessage.java
@@ -6,13 +6,13 @@ public class GreetingMessage {
 	public String getGreeting(String name) {
 		int  where=AMorPM();
 		if(where==Calendar.AM) {
-			return "Good Morning " + name + "!!!";
+			return "Good Morning " + name + "@@@";
 		} 
 		else if(where==Calendar.PM) {
-			return "Good Night " + name + "!!!";
+			return "Good Night " + name + "@@@";
 		} 
 		else {
-			return "Hello " + name + "!!!";
+			return "Hello " + name + "@@@";
 		}
 	}
 	public int AMorPM() {
